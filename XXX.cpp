89fedf5f197dcbb77904f8443eb6e99c9652010b
wutong <wutongtoby@gmail.com>
2020-05-10 16:33:41 +0800

    add the other file

---
+++ XXX.cpp
@@ -0,0 +1,61 @@
+#include "mbed.h"
+#include "DA7212.h"
+#include "uLCD_4DGL.h"
+
+// will eventqueue pop up the task after the former task is completed?
+// if so, we can actually put sound_and_ulcd, mode_selection, sound_selection 
+// in the same thread, since they all won't funciton at the same time?
+
+// maybe length_array, onte_array, can just be 2-dimensional array
+// so that we just need to indicate which 1-dimensional array we want to play?
+
+int *length_array;
+int *tone_array;
+int number_of_note;
+int *table;
+bool play_on, taiko_on;
+void sound_and_ulcd(void); 
+// the function is used to 
+//	1. play the sound from table + length_array + tone_array
+//	2. if play_on then play sound and vice versa
+//	3. show song info on the uLCD
+//	4. display taiko pattern if taiko_on
+// and the function will be in a single thread with lower priority
+// once the play_on is false, the function will clear the screen and return()
+
+void pause(void);
+// when the switch is push, we will use this function set taiko_on, 
+// play_on to false so that the soound_and_ulcd fuction will 
+// no longer play sound and show information 
+// then, it will add the mode_selection function into the eventqueue
+
+int which_modeORsong; // control by DNN
+void mode_selection(void);
+// there is a infinite loop detecting whether another swith is pressed.
+// If the switch is pressed then if 
+//	1. which_modeORsong shows that the mode is backward or forward, 
+//	then we will just modify the length_array, tone_array,
+//	number_of_note, and set play_on to true, and add the sound_and_ulcd
+//	function to it's thread's eventqueue, then return()
+//	
+//	2. which_modeORsong shows that the mode is song_selection,
+//	we will just add the song_selection into the eventqueue then return()
+//
+//	3. which_modeORsong shows that the mode is taiko,
+//	we will just do the same thing as back_ward, forward, but this time we 
+//	will set the taiko_on to true and ..... then return()
+//	
+//	4. which_modeORsong shows that the mode is load, then we will send a signal to python
+//	to get the new songs. Everytime we call python, it will just send new three songs
+//	and the three songs will replace the old three songs, so there is no need to indicate 
+//	which songs we want! After loading the new songs, we will just go back to the infinite loop.
+// Everytime excute the loop, we will refresh the ulcd.
+
+void song_selection(void);
+// there is a infinite loop detecting another switch is pressed too,
+// if the switch is pressed, then we will see that what which_modeORsong
+// is to modify tone_array, length_array, number_of_note and set play_on
+// and add the sound_and_ulcd into its thread, then return()
+
+void set_which_modeORsong;
+// a funciton running in another thread and will use DNN 
--- main.cpp
+++ main.cpp
@@ -1,57 +1,166 @@
-// will eventqueue pop up the task after the former task is completed?
-// if so, we can actually put sound_and_ulcd, mode_selection, sound_selection 
-// in the same thread, since they all won't funciton at the same time?
+#include "accelerometer_handler.h"
+#include "config.h"
+#include "magic_wand_model_data.h"
+#include "tensorflow/lite/c/common.h"
+#include "tensorflow/lite/micro/kernels/micro_ops.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
 
-// maybe length_array, onte_array, can just be 2-dimensional array
-// so that we just need to indicate which 1-dimensional array we want to play?
+Thread t1(osPriorityNormal, 120 * 1024 /*120K stack size*/);
 
-int *length_array;
-int *tone_array;
-int number_of_note;
-int *table;
-bool play_on, taiko_on;
-void sound_and_ulcd(void); 
-// the function is used to 
-//	1. play the sound from table + length_array + tone_array
-//	2. if play_on then play sound and vice versa
-//	3. show song info on the uLCD
-//	4. display taiko pattern if taiko_on
-// and the function will be in a single thread with lower priority
-// once the play_on is false, the function will clear the screen and return()
+// Return the result of the last prediction
+int PredictGesture(float* output) {
+    // How many times the most recent gesture has been matched in a row
+    static int continuous_count = 0;
+    // The result of the last prediction
+    static int last_predict = -1;
 
-void pause(void);
-// when the switch is push, we will use this function set taiko_on, 
-// play_on to false so that the soound_and_ulcd fuction will 
-// no longer play sound and show information 
-// then, it will add the mode_selection function into the eventqueue
+    // Find whichever output has a probability > 0.8 (they sum to 1)
+    int this_predict = -1;
+    for (int i = 0; i < label_num; i++) {
+        if (output[i] > 0.8) this_predict = i;
+    }
 
-int which_modeORsong; // control by DNN
-void mode_selection(void);
-// there is a infinite loop detecting whether another swith is pressed.
-// If the switch is pressed then if 
-//	1. which_modeORsong shows that the mode is backward or forward, 
-//	then we will just modify the length_array, tone_array,
-//	number_of_note, and set play_on to true, and add the sound_and_ulcd
-//	function to it's thread's eventqueue, then return()
-//	
-//	2. which_modeORsong shows that the mode is song_selection,
-//	we will just add the song_selection into the eventqueue then return()
-//
-//	3. which_modeORsong shows that the mode is taiko,
-//	we will just do the same thing as back_ward, forward, but this time we 
-//	will set the taiko_on to true and ..... then return()
-//	
-//	4. which_modeORsong shows that the mode is load, then we will send a signal to python
-//	to get the new songs. Everytime we call python, it will just send new three songs
-//	and the three songs will replace the old three songs, so there is no need to indicate 
-//	which songs we want! After loading the new songs, we will just go back to the infinite loop.
-// Everytime excute the loop, we will refresh the ulcd.
+    // No gesture was detected above the threshold
+    if (this_predict == -1) {
+        continuous_count = 0;
+        last_predict = label_num;
+        return label_num;
+    }
 
-void song_selection(void);
-// there is a infinite loop detecting another switch is pressed too,
-// if the switch is pressed, then we will see that what which_modeORsong
-// is to modify tone_array, length_array, number_of_note and set play_on
-// and add the sound_and_ulcd into its thread, then return()
+    if (last_predict == this_predict) {
+        continuous_count += 1;
+    } 
+    else {
+        continuous_count = 0;
+    }
+    last_predict = this_predict;
 
-void set_which_modeORsong;
-// a funciton running in another thread and will use DNN 
+    // If we haven't yet had enough consecutive matches for this gesture,
+    // report a negative result
+    if (continuous_count < config.consecutiveInferenceThresholds[this_predict]) {
+        return label_num;
+    }
+    // Otherwise, we've seen a positive result, so clear all our variables
+    // and report it
+    continuous_count = 0;
+    last_predict = -1;
+
+    return this_predict;
+}
+
+
+void DNN(void) {
+    // Create an area of memory to use for input, output, and intermediate arrays.
+    // The size of this will depend on the model you're using, and may need to be
+    // determined by experimentation.
+    constexpr int kTensorArenaSize = 60 * 1024;
+    uint8_t tensor_arena[kTensorArenaSize];
+
+    // Whether we should clear the buffer next time we fetch data
+    bool should_clear_buffer = false;
+    bool got_data = false;
+
+    // The gesture index of the prediction
+    int gesture_index;
+
+    // Set up logging.
+    static tflite::MicroErrorReporter micro_error_reporter;
+    tflite::ErrorReporter* error_reporter = &micro_error_reporter;
+
+    // Map the model into a usable data structure. This doesn't involve any
+    // copying or parsing, it's a very lightweight operation.
+    const tflite::Model* model = tflite::GetModel(g_magic_wand_model_data);
+    if (model->version() != TFLITE_SCHEMA_VERSION) {
+        error_reporter->Report(
+            "Model provided is schema version %d not equal "
+            "to supported version %d.",
+            model->version(), TFLITE_SCHEMA_VERSION);
+        return -1;
+    }
+
+    // Pull in only the operation implementations we need.
+    // This relies on a complete list of all the ops needed by this graph.
+    // An easier approach is to just use the AllOpsResolver, but this will
+    // incur some penalty in code space for op implementations that are not
+    // needed by this graph.
+    static tflite::MicroOpResolver<6> micro_op_resolver;
+    micro_op_resolver.AddBuiltin(
+        tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
+        tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
+    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_MAX_POOL_2D,
+                                tflite::ops::micro::Register_MAX_POOL_2D());
+    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
+                                tflite::ops::micro::Register_CONV_2D());
+    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_FULLY_CONNECTED,
+                                tflite::ops::micro::Register_FULLY_CONNECTED());
+    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_SOFTMAX,
+                                tflite::ops::micro::Register_SOFTMAX());
+    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_RESHAPE,
+                                tflite::ops::micro::Register_RESHAPE(), 1);
+    // Build an interpreter to run the model with
+    static tflite::MicroInterpreter static_interpreter(
+        model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
+    tflite::MicroInterpreter* interpreter = &static_interpreter;
+
+    // Allocate memory from the tensor_arena for the model's tensors
+    interpreter->AllocateTensors();
+
+    // Obtain pointer to the model's input tensor
+    TfLiteTensor* model_input = interpreter->input(0);
+    if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
+        (model_input->dims->data[1] != config.seq_length) ||
+        (model_input->dims->data[2] != kChannelNumber) ||
+        (model_input->type != kTfLiteFloat32)) {
+        error_reporter->Report("Bad input tensor parameters in model");
+        return -1;
+    }
+
+    int input_length = model_input->bytes / sizeof(float);
+
+    TfLiteStatus setup_status = SetupAccelerometer(error_reporter);
+    if (setup_status != kTfLiteOk) {
+        error_reporter->Report("Set up failed\n");
+        return -1;
+    }
+
+    error_reporter->Report("Set up successful...\n");
+
+    while (true){
+
+        // Attempt to read new data from the accelerometer
+        got_data = ReadAccelerometer(error_reporter, model_input->data.f,
+                                    input_length, should_clear_buffer);
+
+        // If there was no new data,
+        // don't try to clear the buffer again and wait until next time
+        if (!got_data) {
+            should_clear_buffer = false;
+            continue;
+        }
+
+        // Run inference, and report any error
+        TfLiteStatus invoke_status = interpreter->Invoke();
+        if (invoke_status != kTfLiteOk) {
+            error_reporter->Report("Invoke failed on index: %d\n", begin_index);
+            continue;
+        }
+
+        // Analyze the results to obtain a prediction
+        gesture_index = PredictGesture(interpreter->output(0)->data.f);
+
+        // Clear the buffer next time we read data
+        should_clear_buffer = gesture_index < label_num;
+
+        // Produce an output
+        if (gesture_index < label_num) {
+            error_reporter->Report(config.output_message[gesture_index]);
+        }
+    }
+}
+int main(void) {
+    t1.start(DNN);
+}
